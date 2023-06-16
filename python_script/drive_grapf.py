d8fb39094a34f08d83d7da99ff463a25fd5f2ce4
Ayan <xchjjvg07@gmail.com>
2023-06-17 05:45:07 +0600

    images added, python script updated

--- python_script/drive_grapf.py
+++ python_script/drive_grapf.py
@@ -8,8 +8,18 @@ place_name = "Nur-Sultan, Kazakhstan"
 # Получаем граф дорог для указанного местоположения
 graph = ox.graph_from_place(place_name, network_type='all')
 
-# Отображаем ребра графа дорог
-fig, ax = ox.plot_graph(graph, edge_color='white', edge_linewidth=0.5, node_size=0, show=False)
+# Создайте копию графа
+filtered_graph = graph.copy()
 
-# Показываем граф
+# Итерируйтесь по каждому ребру графа
+for u, v, k, data in graph.edges(keys=True, data=True):
+    # Проверьте тип дороги на пешеходные тропы
+    if "footway" not in data["highway"] and "path" not in data["highway"]:
+        # Если тип дороги не является пешеходной тропой, удаляем ребро из графа
+        filtered_graph.remove_edge(u, v, k)
+
+# Отображаем ребра графа дорог
+fig, ax = ox.plot_graph(filtered_graph, edge_color='white', edge_linewidth=0.5, node_size=0, show=False)
+
+# Показываем граф 
 plt.show()
