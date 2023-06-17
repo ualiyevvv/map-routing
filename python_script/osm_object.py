c3252e16b3717700522f274eacef7ba052fd3f01
Ayan <xchjjvg07@gmail.com>
2023-06-17 06:00:36 +0600

    images added, python script updated

--- python_script/osm_object.py
+++ python_script/osm_object.py
@@ -17,28 +17,28 @@ tags = {'leisure': True}
 gdf = ox.geometries.geometries_from_address(place_name, tags, dist=10000)
 
 # Convert GeoDataFrame to a pandas DataFrame to handle NaN values
-df = pd.DataFrame(gdf)
+# df = pd.DataFrame(gdf)
 
-# Replace NaN values with None or any other suitable representation
-df = df.where(pd.notnull(df), None)
+# # Replace NaN values with None or any other suitable representation
+# df = df.where(pd.notnull(df), None)
 
-# Create an empty list to store the feature properties
-features = []
+# # Create an empty list to store the feature properties
+# features = []
 
-# Iterate over the rows of the DataFrame
-for idx, row in df.iterrows():
-    # Extract the properties of each row
-    properties = row.drop('geometry').to_dict()
+# # Iterate over the rows of the DataFrame
+# for idx, row in df.iterrows():
+#     # Extract the properties of each row
+#     properties = row.drop('geometry').to_dict()
     
-    # Create a feature dictionary with properties and geometry
-    feature = {
-        "type": "Feature",
-        "properties": properties,
-        "geometry": row['geometry'].__geo_interface__
-    }
+#     # Create a feature dictionary with properties and geometry
+#     feature = {
+#         "type": "Feature",
+#         "properties": properties,
+#         "geometry": row['geometry'].__geo_interface__
+#     }
     
-    # Add the feature to the list
-    features.append(feature)
+#     # Add the feature to the list
+#     features.append(feature)
 # features = []
 # nodes_attributes = {}
 # # Plot the filtered points of interest
@@ -58,14 +58,14 @@ gdf.plot(ax=plt.gca(), color='red', alpha=0.6, markersize=10)
 #     features.append(feature)
 
 # Create the feature collection dictionary
-feature_collection = {
-    "type": "FeatureCollection",
-    "features": features
-}
-# Save the GeoJSON data to a file
-filename = f"gdf_attributes2.json"
-with open(filename, "w") as json_file:
-    json.dump(feature_collection, json_file)
+# feature_collection = {
+#     "type": "FeatureCollection",
+#     "features": features
+# }
+# # Save the GeoJSON data to a file
+# filename = f"gdf_attributes2.json"
+# with open(filename, "w") as json_file:
+#     json.dump(feature_collection, json_file)
 
 # Отображаем карту
 plt.show()
